2b719d45c18067b9c35242aef2c1cd0c50b94bb2
KareePe <perapat8589@gmail.com>
2024-05-10 19:54:40 +0700

    meta and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,20 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app:{
+    head:{
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "theme-color", content: "#BF2C7B" },
+        { name: "msapplication-navbutton-color", content: "#BF2C7B" },
+        { name: "apple-mobile-web-app-status-bar-style", content: "#BF2C7B" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "images/logo/symbol/square.png" },
+      ]
+    }
+  },
   modules: ["@nuxtjs/i18n",'@vueform/nuxt'],
   i18n: {
     strategy: "no_prefix",
